2a32bb541fff1f1caaeaa53aa0dd187444de421b
Vamsi krishna <vamsikrishna@vamsis-MacBook-Pro.local>
2025-07-05 17:40:31 +0530

    Final version Sat Jul  5 17:40:31 IST 2025

---
+++ script.js
@@ -0,0 +1,22 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    console.log("whoops, there is an error form async: ", error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  button.disabled = true;
+  await videoElement.requestPictureInPicture();
+  button.disabled = false;
+});
+// on load
+selectMediaStream();
